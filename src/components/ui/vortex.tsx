d04917d16fbfe719cb5c76195a9688f41232691d
Prajyot Tayde <pmtbmt@gmail.com>
2025-05-18 12:43:32 +0530

    fix contact page bug (nav was unclickable)

--- src/components/ui/vortex.tsx
+++ src/components/ui/vortex.tsx
@@ -233,7 +233,7 @@ export const Vortex = (props: VortexProps) => {
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         ref={containerRef}
-        className="absolute h-full w-full inset-0 z-0 bg-transparent flex items-center justify-center"
+        className="absolute h-full w-full inset-0 z-0 bg-transparent flex items-center justify-center pointer-events-none"
       >
         <canvas ref={canvasRef}></canvas>
       </motion.div>
